84d8f23527c2c3dcd9502c9ac32617443b23025c
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-03-09 15:21:05 +0100

    .

--- src/main/java/com/ricky/metalbox/MetalboxApp.java
+++ src/main/java/com/ricky/metalbox/MetalboxApp.java
@@ -9,7 +9,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MetalboxApp {
+public class MetalboxApp extends Application{
     @Override
     public void start(Stage primaryStage) {
         // 1. Inizializza il Modello
