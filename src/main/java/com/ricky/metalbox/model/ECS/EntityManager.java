6b1a1d25d74586a4d255b97c0e108a054b379eb8
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-03-18 16:26:33 +0100

    add entitymanager, still missing frienshipComponent in components

---
+++ src/main/java/com/ricky/metalbox/model/ECS/EntityManager.java
@@ -0,0 +1,42 @@
+package com.ricky.metalbox.model.ECS;
+
+import com.ricky.metalbox.model.ECS.Components.TargetComponent;
+import com.ricky.metalbox.model.ECS.Components.ThinkingComponent;
+import com.ricky.metalbox.model.ECS.Components.ShapeComponent;
+import com.ricky.metalbox.model.ECS.Components.PositionComponent;
+
+public class EntityManager {
+
+    public static final int MAX_ENTITIES = 50000;
+
+    private int nextEntityId = 0;
+
+    //serie di array paralleli in cui l'indice corrisponde all'id dell'entità
+    public final PositionComponent[] positionComponents = new PositionComponent[MAX_ENTITIES];
+    public final ShapeComponent[] shapeComponents = new ShapeComponent[MAX_ENTITIES];
+    public final TargetComponent[] targetComponents = new TargetComponent[MAX_ENTITIES];
+    public final ThinkingComponent[] thinkingComponents = new ThinkingComponent[MAX_ENTITIES];
+
+    public final boolean[] isAlive = new boolean[MAX_ENTITIES];
+
+    public EntityManager() {
+        //tutto empty
+    }
+
+    public int createEntity() {
+        if (nextEntityId >= MAX_ENTITIES) {
+            throw new RuntimeException("max entities limit reached");
+        }
+        int id = nextEntityId++; // per crearla basta muoversi avanti nell'array preallocato
+        isAlive[id] = true;
+        return id;
+    }
+
+    public void destroyEntity(final int id) {
+        isAlive[id] = false;
+        positionComponents[id] = null;
+        shapeComponents[id] = null;
+        targetComponents[id] = null;
+        thinkingComponents[id] = null;
+    }
+}
